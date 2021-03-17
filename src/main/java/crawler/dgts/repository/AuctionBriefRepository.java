4ddda934f117c45017855d273f13e0090273ce25
Hoang Pham Dinh <phamdinhhoang.fetk54@gmail.com>
2021-03-17 23:29:21 +0700

    compare CLOB column by dbms_lob.compare

--- src/main/java/crawler/dgts/repository/AuctionBriefRepository.java
+++ src/main/java/crawler/dgts/repository/AuctionBriefRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 import crawler.dgts.entity.AuctionBriefInfo;
 
 public interface AuctionBriefRepository extends CrudRepository<AuctionBriefInfo, Integer> {
-	@Query(value="SELECT COUNT(*) FROM AUCTION_BRIEF_INFO WHERE LINK_LOCATION IS NOT NULL AND LINK_LOCATION = :link", nativeQuery=true)
+	@Query(value="SELECT COUNT(*) FROM AUCTION_BRIEF_INFO WHERE LINK_LOCATION IS NOT NULL AND  dbms_lob.compare(LINK_LOCATION, :link) = 0", nativeQuery=true)
 	Integer countExistedLink(@Param ("link") String link);
 
 }
